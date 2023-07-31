052537627d035e7a93e87ee8741f6c2ba2b0de24
Isaac Yeo <isaacyeo93@gmail.com>
2023-07-31 21:46:02 +0300

    New Poll UI

--- my-app/src/components/NewQuestion.js
+++ my-app/src/components/NewQuestion.js
@@ -1,5 +1,57 @@
+import { useState } from "react";
+
 const NewQuestion = () => {
-  return <div>NewQuestion</div>
+  const [optionOne, setoptionOne] = useState("");
+  const [optionTwo, setoptionTwo] = useState("");
+
+  const handleoptionOne = (e) => {
+    const text = e.target.value;
+    setoptionOne(text);
+  };
+
+  const handleoptionTwo = (e) => {
+    const text = e.target.value;
+    setoptionTwo(text);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // todo: Add Question to store
+    console.log("Option one: ", optionOne);
+    console.log("Option two: ", optionTwo);
+    setoptionOne("");
+    setoptionTwo("");
+  };
+
+  return (
+    <div>
+      <h1 className="center">Would You Rather</h1>
+      <p className="center">Create Your Own Poll</p>
+      <form className="new-question" onSubmit={handleSubmit}>
+        {/* todo: Redirect to / if submitted */}
+        <h3 className="center">First Option</h3>
+        <textarea
+          placeholder="Option One"
+          value={optionOne}
+          onChange={handleoptionOne}
+          className="textarea"
+          maxLength={280}
+        />
+        <h3 className="center">Second Option</h3>
+        <textarea
+          placeholder="Option Two"
+          value={optionTwo}
+          onChange={handleoptionTwo}
+          className="textarea"
+          maxLength={280}
+        />
+        <button className="btn" type="submit" disabled={optionOne === "" || optionTwo === ""}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default NewQuestion;
