fa397743d3a006c3b2f36cadabfae1345e47e914
Isaac Yeo <isaacyeo93@gmail.com>
2023-08-18 00:53:28 +0800

    Unanswered questions are shown by default and user can toggle between answered and unanswered polls

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import Question from "./Question";
 
-const Dashboard = (props) => {  
+const Dashboard = (props) => {
+  // https://techwithcy.com/blog/2022-08-17-simple-toggle-view/
+  const [isToggle, setIsToggle] = useState(true);
+  
+  const toggleChange = () => {
+    setIsToggle(!isToggle)
+  }
+
   const doneQns = props.questionIds.filter((id) => (
     props.questions[id].optionOne.votes.includes(props.authedUser) || 
     props.questions[id].optionTwo.votes.includes(props.authedUser)
@@ -16,22 +24,49 @@ const Dashboard = (props) => {
 
   return (
     <div>
-      <h3 className="center">New Questions</h3>
-      <ul className="dashboard-list">
-        {newQns.map((id) => (
-          <li key={id}>
-            <Question id={id} />
-          </li>
-        ))}
-      </ul>
-      <h3 className="center">Done</h3>
-      <ul className="dashboard-list">
-        {doneQns.map((id) => (
-          <li key={id}>
-            <Question id={id} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input
+          type="radio"
+          value={true}
+          name="toggle"
+          checked={isToggle}
+          onChange={toggleChange}
+        />
+        <label>New Questions</label>
+        <input
+          type="radio"
+          value={false}
+          name="toggle"
+          onChange={toggleChange}
+        />
+        <label>Done</label>
+      </div>
+
+      {isToggle && (
+        <div>
+          <h3 className="center">New Questions</h3>
+          <ul className="dashboard-list">
+            {newQns.map((id) => (
+              <li key={id}>
+                <Question id={id} />
+              </li>
+            ))}
+          </ul>
+        </div>   
+      )}
+
+      {!isToggle && (
+        <div>
+          <h3 className="center">Done</h3>
+          <ul className="dashboard-list">
+            {doneQns.map((id) => (
+              <li key={id}>
+                <Question id={id} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
